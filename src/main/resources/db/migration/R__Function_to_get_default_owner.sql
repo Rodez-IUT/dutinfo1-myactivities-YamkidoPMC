ac3b964a50d58bbffdecb55e18826be0d105239b
Pierre-Marie Combalbert <pierremarie.combalbert@iut-rodez.fr>
2019-03-13 17:59:02 +0100

    Partie 4 fct 1

---
+++ src/main/resources/db/migration/R__Function_to_get_default_owner.sql
@@ -0,0 +1,13 @@
+CREATE OR REPLACE FUNCTION get_default_owner() RETURNS "user" AS $$
+	DECLARE
+		defaultOwner "user"%rowtype;
+		defaultOwnerUsername varchar(500) := 'Default Owner';
+	BEGIN	
+		SELECT * INTO defaultOwner FROM "user" WHERE username = defaultOwnerUsername;
+		IF NOT FOUND THEN
+			INSERT INTO "user" (id,username) VALUES (nextval('id_generator'), defaultOwnerUsername);
+			SELECT * INTO defaultOwner FROM "user" WHERE username = defaultOwnerUsername;
+		END IF;
+		RETURN defaultOwner;
+	END
+$$ LANGUAGE PLPGSQL;
